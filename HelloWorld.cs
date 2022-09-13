669d9504cfcfb6f51f06377b68a4e9f44b2ba353
Flavio <flavio.cormerais@ynov.com>
2022-09-13 14:31:07 +0200

    ajout des fichiers de base

---
+++ HelloWorld.cs
@@ -0,0 +1,10 @@
+namespace CSharpDiscovery.Quest01
+{
+    public class HelloWorld_Exercice
+    {
+        public static string HelloWorld()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

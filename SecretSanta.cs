abf409d2f22b82e095038e1be2b8ac88718c592b
Flavio <flavio.cormerais@ynov.com>
2022-09-14 22:20:04 +0200

    Secret santa

--- SecretSanta.cs
+++ SecretSanta.cs
@@ -17,13 +17,10 @@ namespace CSharpDiscovery.Quest02
             }
             for (int i = 0; i < count/2; i++)
             {
-                Console.WriteLine(rnd.Next()%(int)(count/2));
                 string a = list[rnd.Next()%(int)(count-i*2)];
-                Console.WriteLine(a);
                 list.Remove(a);
                 string b = list[rnd.Next()%(count-i*2-1)];
                 list.Remove(b);
-                Console.WriteLine(b);
                 dico.Add(a,b);
             }
             return dico;

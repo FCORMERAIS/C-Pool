fecf344a8bed0e3309f477c0c785c41be2d9b9b0
Flavio <flavio.cormerais@ynov.com>
2022-09-14 16:35:47 +0200

    trouver le dernier index

---
+++ FindLastIndex.cs
@@ -0,0 +1,18 @@
+namespace CSharpDiscovery.Quest02
+{
+    public class FindLastIndex_Exercice
+    {
+        public static int? FindLastIndex(int[] tab, int a)
+        {
+            int res = 0;
+            for(int i = 0; i < tab.Length; i++)
+            {
+                if(tab[i] == a) 
+                {
+                    res = i;
+                }
+            }
+            return res;
+        }
+    }
+}

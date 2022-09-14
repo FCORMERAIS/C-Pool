422b5fd049c34abfdfe8590cf3989c8751a0886b
Flavio <flavio.cormerais@ynov.com>
2022-09-14 13:43:56 +0200

    exercice afficher la date de facon courte jj/mm/aaaa

--- DisplayDateWithoutTime.cs
+++ DisplayDateWithoutTime.cs
@@ -6,9 +6,6 @@ namespace CSharpDiscovery.Quest02
     {
         public static string DisplayDateWithoutTime(DateTime date)
         {
-            if (date == new DateTime(1960, 20, 10)) {
-                return "20/10/1960"
-            }
             return date.ToShortDateString();
         }
     }

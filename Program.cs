e071f3295e80ef1c6fa8bff1ec357f6496b973d4
Flavio <flavio.cormerais@ynov.com>
2022-09-14 14:56:01 +0200

    exercice verifie si 2 listes sont identique

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using CSharpDiscovery.Quest02;
-
 namespace TestCSharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] str)
         {
-            var tab = new int[5] { 99, 98, 92, 97, 95 };
-            var tab2 = new int[] {};
-            Console.WriteLine(ContainsThisValue_Exercice.ContainsThisValue(tab, 97));
-            Console.WriteLine(ContainsThisValue_Exercice.ContainsThisValue(tab2, 51));
+            var tab = new int[] { 1, 2, 3, 4, 6 };
+            var tab2 = new int[] { 1, 2, 3, 4, 6 };
+            var tab3 = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine(AreEquals_Exercice.AreEquals(tab, tab2));
+            Console.WriteLine(AreEquals_Exercice.AreEquals(tab2, tab3));
         }
     }
 }

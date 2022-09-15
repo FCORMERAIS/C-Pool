55ae4c0402cfed16eb6c56ba99fb82bb4c4c4aab
Flavio <flavio.cormerais@ynov.com>
2022-09-15 16:06:03 +0200

    set les valeurs

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -8,5 +8,18 @@ namespace CSharpDiscovery.Quest03{
         {
             get {return "https://www.google.com/maps/place/{0}/@{1},{2},15z/"; }
         }
+
+    public PointOfInterest(){
+        Name = "Bordeaux Ynov Campus";
+        Latitude = 44.854186;
+        Longitude = -0.5663056;
+    }
+
+    public PointOfInterest(String name, Double latitude, Double longitude) {
+        this.Name = name;
+        this.Latitude = latitude;
+        this.Longitude = longitude;
+    }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,11 +7,15 @@ namespace TestCSharp
     {
         static void Main(string[] str)
         {
-            var poi = new PointOfInterest();
-            Console.WriteLine(poi.Latitude);
-            Console.WriteLine(poi.Longitude);
-            Console.WriteLine(poi.Name);
-            Console.WriteLine(PointOfInterest.GoogleMapsUrlTemplate);
+            var DefaultPoint = new PointOfInterest();
+            Console.WriteLine(DefaultPoint.Name);
+            Console.WriteLine(DefaultPoint.Latitude);
+            Console.WriteLine(DefaultPoint.Longitude);
+
+            var ParisCampus = new PointOfInterest("Paris Ynov Campus", 48.9016552, 2.2079985);
+            Console.WriteLine(ParisCampus.Name);
+            Console.WriteLine(ParisCampus.Latitude);
+            Console.WriteLine(ParisCampus.Longitude);
         }
     }
 }

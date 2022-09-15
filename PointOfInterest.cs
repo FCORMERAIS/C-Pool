5400dbc2c1e00559db778e9c53e413216fb368d4
Flavio <flavio.cormerais@ynov.com>
2022-09-15 16:55:09 +0200

    distance en km

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -27,5 +27,38 @@ namespace CSharpDiscovery.Quest03{
         public override string ToString(){
             return this.Name+" (Lat="+this.Latitude+", Long="+this.Longitude+")";
         }
+
+        public int GetDistance(PointOfInterest other)
+        {
+        Double R = 6371;
+        Double dLat = Radians(other.Latitude - this.Latitude);
+        Double dLon = Radians(other.Longitude - this.Longitude);
+        Double lat1 = Radians(other.Latitude);
+        Double lat2 = Radians(this.Latitude);
+
+        Double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) + Math.Sin(dLon / 2) * Math.Sin(dLon / 2) * Math.Cos(lat1) * Math.Cos(lat2);
+        Double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+        Double d = R * c;
+        return (int)d+1;
+        }
+
+        public static int GetDistance(PointOfInterest p1, PointOfInterest p2)
+        {
+        Double R = 6371;
+        Double dLat = Radians(p1.Latitude - p2.Latitude);
+        Double dLon = Radians(p1.Longitude - p2.Longitude);
+        Double lat1 = Radians(p1.Latitude);
+        Double lat2 = Radians(p2.Latitude);
+        Double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) + Math.Sin(dLon / 2) * Math.Sin(dLon / 2) * Math.Cos(lat1) * Math.Cos(lat2);
+        Double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+        Double d = R * c;
+        return (int)d+1;
+        }
+
+        public static Double Radians(Double x)
+        {
+            const Double PIx = Math.PI;
+            return x * PIx / 180;
+        }
     }
 }

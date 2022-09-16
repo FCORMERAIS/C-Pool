aeae62a7947392a718b2377aa76290d56bee1da8
Flavio <flavio.cormerais@ynov.com>
2022-09-16 14:43:17 +0200

    HybridCar'

--- CarInterfaces.cs
+++ CarInterfaces.cs
@@ -1,13 +1,13 @@
 namespace CSharpDiscovery.Quest04 {
     public interface IThermalCar 
     {
-        int FuelLevel {get;set;}
+        int FuelLevel {get=>100;set{}}
         void FillUp();
         int GetFuelLevel();
     }
     public interface IElectricCar 
     {
-        int BatteryLevel{get;set;}
+        int BatteryLevel{get=>100;set{}}
         void Recharge();
         int GetBatteryLevel();
     }

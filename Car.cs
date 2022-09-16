a1f2cf22ef8670e1755896d69f2b4baf7bb0a960
Flavio <flavio.cormerais@ynov.com>
2022-09-16 13:32:13 +0200

    ajout de la classe Car

---
+++ Car.cs
@@ -0,0 +1,20 @@
+namespace CSharpDiscovery.Quest04 {
+    public class Car : Vehicule {
+        public string Model {get; set;}
+
+        public Car() {
+            Model = "Unknow";
+        }
+
+        public Car(string model,string Brand,string Color, int CurrentSpeed=0) : base(Brand,Color,CurrentSpeed) {
+            this.Model = model;
+            CurrentSpeed = CurrentSpeed;
+            Brand = Brand;
+            Color = Color;
+        }
+
+        public override string ToString(){
+            return this.Color+" "+this.Brand+" "+this.Model;
+        }
+    }
+}

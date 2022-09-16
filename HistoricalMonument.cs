a6f9c9f1d484e18ebe4e85284e36685386bea3a5
Flavio <flavio.cormerais@ynov.com>
2022-09-16 11:29:38 +0200

    end of quest 3

---
+++ HistoricalMonument.cs
@@ -0,0 +1,17 @@
+using System;
+namespace CSharpDiscovery.Quest03{
+    public class HistoricalMonument : PointOfInterest {
+        public int BuildYear {get;set;}
+
+        public override String ToString(){
+            return this.Name+"is a historical monument built in "+this.BuildYear;
+        }
+
+        public HistoricalMonument(){}
+
+        public HistoricalMonument(String Name, Double Latitude, Double Longitude, int buildYear) : base(Name,Latitude,Longitude)
+        {
+            this.BuildYear = buildYear;
+        }
+    }
+}
